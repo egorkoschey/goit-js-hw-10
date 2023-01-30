a2a3ea687a52ad79a59a5d1a6513522f6189e97a
egorkoschey <egor.koschey@gmail.com>
2023-01-30 19:12:38 +0200

    fixes

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,9 +1,12 @@
 export function fetchCountries(name) {
     return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
-        .then(response => {
-            if (response.ok) {
-                return response.json();
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
             }
-            throw new Error(response.statusText);
-        });
+            return response.json();
+        }).catch(function (error) {
+        console.log('404:' + error);
+      });
+    
 };
